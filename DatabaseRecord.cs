e63470c5744f978fc9b5eac3817854c59e82ff6c
Per Lundberg <per.lundberg@ecraft.com>
2018-03-23 21:11:13 +0200

    Made the Pg2Couch project usable as a library (#4)
    * Made the Pg2Couch project usable as a library
    * Added more methods, needed for transformations
    * Removed commented out code
    * Avoid creating lambda when method can be cast to Func right away

---
+++ DatabaseRecord.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using Humanizer;
+
+namespace Pg2Couch
+{
+    public class DatabaseRecord : Dictionary<string, object>
+    {
+        public DatabaseRecord TransformKeys(Transformation transformation)
+        {
+            var databaseRecord = new DatabaseRecord();
+
+            if (transformation == Transformation.CamelizeLower)
+            {
+                foreach (var key in Keys)
+                {
+                    databaseRecord[key.Camelize()] = this[key];
+                }
+
+                return databaseRecord;
+            }
+            else if (transformation == Transformation.CamelizeUpper)
+            {
+                foreach (var key in Keys)
+                {
+                    databaseRecord[key.Pascalize()] = this[key];
+                }
+
+                return databaseRecord;
+            }
+            else if (transformation == Transformation.SnakeCase)
+            {
+                foreach (var key in Keys)
+                {
+                    databaseRecord[key.Underscore()] = this[key];
+                }
+
+                return databaseRecord;
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public object RemoveAndReturn(string key)
+        {
+            var result = this[key];
+            Remove(key);
+            return result;
+        }
+
+        public DatabaseRecord Transform(Action<DatabaseRecord> transformer)
+        {
+            transformer(this);
+            return this;
+        }
+
+        internal static DatabaseRecord FromReader(DbDataReader reader, List<string> columnNames)
+        {
+            var databaseRecord = new DatabaseRecord();
+
+            for (int i = 0; i < columnNames.Count; i++)
+            {
+                databaseRecord[columnNames[i]] = reader[i];
+            }
+
+            return databaseRecord;
+        }
+    }
+}
---
+++ Transformation.cs
@@ -0,0 +1,21 @@
+﻿namespace Pg2Couch
+{
+    public enum Transformation
+    {
+        /// <summary>
+        /// Converts field names to camel-case using lowercase-initial letter. (i.e. orderNumber style)
+        /// </summary>
+        CamelizeLower,
+
+        /// <summary>
+        /// Converts field names to camel-case uppercase-initial letter (also known as Pascal Case), i.e. OrderNumber
+        /// style.
+        /// </summary>
+        CamelizeUpper,
+
+        /// <summary>
+        /// Converts field names to snake case, i.e. order_number style.
+        /// </summary>
+        SnakeCase,
+    }
+}
